9f3629646d995be049dc6cd05134703c354eef2d
carlos <rudelli.quimson@yahoo.com>
2021-03-30 16:16:57 -0400

    added roomDao methods and extractors and rowmapper

---
+++ P1_Manager/src/main/java/com/revature/dao/RoomDaoJdbcTemplate.java
@@ -0,0 +1,94 @@
+package com.revature.dao;
+
+import java.sql.PreparedStatement;
+import java.util.List;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.revature.dao.mapper.RoomRowMapper;
+import com.revature.pojo.Room;
+
+public class RoomDaoJdbcTemplate implements RoomDao {
+
+	private JdbcTemplate jdbcTemplate;
+	
+	private RoomRowMapper roomRowMapper;
+	
+	@Override
+	public void addRoom(Room room) {
+		
+		String sql = "INSERT INTO rooms (room_number, room_type, room_status, room_occupied)"
+				+ "VALUES (?, ?, ?, ?)";
+		
+		jdbcTemplate.update(connection -> {
+			PreparedStatement ps = connection.prepareStatement(sql);
+			ps.setInt(1, room.getRoomNumber());
+			ps.setString(2, room.getRoomType());
+			ps.setString(3, room.getRoomStatus());
+			ps.setBoolean(4, room.isRoomOccupied());
+			return ps;
+		});
+
+	}
+
+	@Override
+	public void removeRoom(Room room) {
+		String sql = "DELETE FROM rooms WHERE room_number = ?";
+		
+		jdbcTemplate.update(connection -> {
+			PreparedStatement ps = connection.prepareStatement(sql);
+			ps.setInt(1, room.getRoomNumber());
+			return ps;
+		});
+
+	}
+
+	@Override
+	public void updateRoom(Room room) {
+		String sql = "UPDATE rooms SET room_type = ? WHERE room_number = ?";
+		
+		jdbcTemplate.update( connection -> {
+			PreparedStatement ps = connection.prepareStatement(sql);
+			ps.setString(1, room.getRoomType());
+			ps.setInt(2, room.getRoomNumber());
+			return ps;
+		});
+
+	}
+
+	@Override
+	public void updateRoomStatus(Room room) {
+		String sql = "UPDATE rooms SET room_status = ? WHERE room_number = ?";
+		
+		jdbcTemplate.update( connection -> {
+			PreparedStatement ps = connection.prepareStatement(sql);
+			ps.setString(1, room.getRoomStatus());
+			ps.setInt(2, room.getRoomNumber());
+			return ps;
+		});
+
+	}
+
+	@Override
+	public void updateRoomOccupied(Room room) {
+		String sql = "UPDATE rooms SET room_occupied = ? WHERE room_number = ?";
+		
+		jdbcTemplate.update( connection -> {
+			PreparedStatement ps = connection.prepareStatement(sql);
+			ps.setBoolean(1, room.isRoomOccupied());
+			ps.setInt(2, room.getRoomNumber());
+			return ps;
+		});
+
+	}
+
+	@Override
+	public List<Room> getAllRooms() {
+		String sql = "SELECT * FROM rooms";
+		
+		List<Room> allRooms = jdbcTemplate.query(sql, roomRowMapper);
+		
+		return allRooms;
+	}
+
+}
